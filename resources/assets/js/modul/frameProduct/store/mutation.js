2340fe7832296e38e8f4ce333f01ad5947d806c0
kirbi <kirbi.simbolon@gmail.com>
2018-08-15 12:08:04 +0700

    mutation

--- resources/assets/js/modul/frameProduct/store/mutation.js
+++ resources/assets/js/modul/frameProduct/store/mutation.js
@@ -2,7 +2,7 @@ const UPDATE_DATA1=function UPDATE_DATA1(state,data){ //state mengakses data yan
     state.data1=data
     //segala operasi dilakukan disini
 }
-//a
+ 
 export default{
     UPDATE_DATA1
 } 
